83b0f6424df000d75b20409175785fd77fe059ad
Kenneth James Uy <uy.kennethjames23@gmail.com>
2020-09-08 21:39:40 +0800

    [WIP] Added DetailController screen

---
+++ iTunesMedia/controllers/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  iTunesMedia
+//
+//  Created by Kenneth James Uy on 9/8/20.
+//  Copyright © 2020 dummy. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
